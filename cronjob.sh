7bfa5eb407d8b2cc73f01502b2de147015f82abf
Vitaly Petrov <v31337@gmail.com>
2020-11-05 09:46:18 +0000

    Added crontab support

---
+++ cronjob.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# crontab -e
+# 0 1 * * * /home/kafeg/dev/ladacsv/cronjob.sh
+
+rm /home/kafeg/dev/ladacsv/cron.log
+
+echo "$(date): Start collecting" > /home/kafeg/dev/ladacsv/cron.log 2>&1
+
+./collect.sh lol >> /home/kafeg/dev/ladacsv/cron.log 2>&1
+
+sleep 5
+
+./push.sh >> /home/kafeg/dev/ladacsv/cron.log
+
+echo "$(date): Finish collecting" >> /home/kafeg/dev/ladacsv/cron.log 2>&1
